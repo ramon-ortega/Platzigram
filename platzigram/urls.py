fe52a5023a1b23dbf6c300a97a35aadd5c0755d0
UxorialColt <ramortega1594@gmail.com>
2020-07-01 14:21:08 -0500

    creamos el models posts y ademas agregamos el hack para poder ver la media

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 #Django
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from posts import views as posts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+
+    path('posts/', posts_views.list_posts)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+"""Posts Views"""
 
-# Create your views here.
+# Django
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def list_posts(requets):
+    """Show the posts list"""
+    return HttpResponse('Aqui va a la lista de posts')
