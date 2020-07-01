59addc1291f13be17fa46b8ac47f230ccfd69799
UxorialColt <ramortega1594@gmail.com>
2020-07-01 14:39:33 -0500

    Agregamos el model posts al dashboard

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,36 @@
+""" Posts admin classes"""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from posts.models import Post 
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin."""
+    list_display = ('id', 'user', 'title', 'photo')
+    list_display_links = ('user',)
+    list_editable = ('title', 'photo')
+    search_fields = ('title', 'user', 'user__username', 'user__email', 'user__first_name', 'user__last_name')
+    list_filter = ('created', 'modified')
+
+    fieldsets = (
+        ('Information',{
+            'fields': (
+                ('user', 'profile'),
+            ),
+        }),
+        ('Post Info',{
+            'fields': (
+                ('title', 'photo'),
+            ),
+        }),
+        ('Metadata', {
+            'fields': 
+                ('created', 'modified'),
+        }),
+    )
+
+    readonly_fields = ('created', 'modified')
+
--- users/admin.py
+++ users/admin.py
@@ -17,7 +17,7 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'phone_number', 'website', 'profile_picture')
     list_display_links = ('id', 'user')
     list_editable = ('phone_number', 'website', 'profile_picture')
-    search_fields = ('user__email', 'user__first_name', 'user__last_name', 'user__username', 'user__email', 'phone_number')
+    search_fields = ('user__email', 'user__first_name', 'user__last_name', 'user__username', 'phone_number')
     list_filter = ('created', 'modified', 'user__is_active', 'user__is_staff')
 
     fieldsets = (
