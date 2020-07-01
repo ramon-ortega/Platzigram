0d8040130784d2d33ad5d67b37dac1c7a8a06bf6
UxorialColt <ramortega1594@gmail.com>
2020-07-01 11:45:29 -0500

    Mejoramos la seccion de dashboard para Profile y relacionamos los campos Users y Profile en el dashboard

--- users/admin.py
+++ users/admin.py
@@ -2,8 +2,10 @@
 
 #Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 #Models
+from django.contrib.auth.models import User
 from users.models import Profile
 
 """ Se puede usar de esta forma """
@@ -17,4 +19,47 @@ class ProfileAdmin(admin.ModelAdmin):
     list_editable = ('phone_number', 'website', 'profile_picture')
     search_fields = ('user__email', 'user__first_name', 'user__last_name', 'user__username', 'user__email', 'phone_number')
     list_filter = ('created', 'modified', 'user__is_active', 'user__is_staff')
+
+    fieldsets = (
+        ('Profile',{
+            'fields': (
+                ('user', 'profile_picture'),
+            ),
+        }),
+        ('Extra Info',{
+            'fields': (
+                ('phone_number', 'website'),
+                ('biography')
+            ),
+        }),
+        ('Metadata', {
+            'fields': (
+                ('created', 'modified'),
+            )
+        }),
+    )
+
+    readonly_fields = ('created', 'modified')
+
+class ProfileInline(admin.StackedInline):
+    """ Profile inline admin for users"""
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin"""
+    inlines = (ProfileInline,)
+
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
