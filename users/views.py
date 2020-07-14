5d9dff405e8ad83246cfcb26edf2b8b82254f555
UxorialColt <ramortega1594@gmail.com>
2020-07-14 16:51:41 -0500

    Agregamos signup para platzigram, los datos ya se ven reflejados en nuestra base de datos y podemos crear usuarios

--- users/views.py
+++ users/views.py
@@ -6,6 +6,14 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+# Errors
+
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
 def login_view(request):
     """Login user."""
     if request.method == 'POST':
@@ -27,3 +35,28 @@ def logout_view(request):
     """Logout user."""
     logout(request)
     return redirect('login')
+
+def signup(request):
+    """Signup user."""
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+
+        if password != password_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Passwords do not match'})
+        
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except 	IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'User already exists'})
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
