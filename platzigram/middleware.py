b9931764eea5edb421d2c719e8dd27619f3acc19
UxorialColt <ramortega1594@gmail.com>
2020-07-21 13:49:06 -0500

    Mejorarmos el sistema de update perfil y modificamos el html para que sean visibles los cambios

--- platzigram/middleware.py
+++ platzigram/middleware.py
@@ -19,9 +19,10 @@ class ProfileCompletionMiddleware:
         is called."""
         
         if not request.user.is_anonymous:
-            profile = request.user.profile
-            if not profile.profile_picture or not profile.biography:
-                if request.path not in [reverse('update_profile'), reverse('logout')]:
-                    return redirect('update_profile')
+            if not request.user.is_staff:
+                profile = request.user.profile
+                if not profile.profile_picture or not profile.biography:
+                    if request.path not in [reverse('update_profile'), reverse('logout')]:
+                        return redirect('update_profile')
         response = self.get_response(request)
         return response
--- users/views.py
+++ users/views.py
@@ -63,4 +63,13 @@ def signup(request):
 
 def update_profile(request):
     """Update a user's profile view."""
-    return render(request, 'users/update_profile.html')
+    profile = request.user.profile
+
+    return render(
+        request = request,
+        template_name = 'users/update_profile.html',
+        context = {
+            'profile': profile,
+            'user': request.user
+        }
+    )
